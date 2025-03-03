a901afcb586e8c5b55e5be60b71c460c8d16a7e0
Kolade <olubanjok@gmail.com>
2025-03-03 23:18:28 +0100

    starting code

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require(`electron`);
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile(`index.html`);
+};
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// Quit when all windows are closed (except on macOS)
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
