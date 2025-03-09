6996660154df3b7fba9640ca8affd13eb18b6900
Kolade <olubanjok@gmail.com>
2025-03-09 12:50:21 +0100

    feat: implement dynamic rendering and audio player functions

--- src/index.js
+++ src/index.js
@@ -3,8 +3,29 @@ const playBtn = document.querySelector(".play");
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
 const audioPlayer = document.querySelector("#audio-player");
+const content = document.querySelector(".main-container");
+const homeNav = document.querySelector(".home");
+const musicNav = document.querySelector(".music");
+let retrievedMusicFiles = [];
+let defaultPath = "";
+let playing = false;
+const playIcon = ` <img src="./assets/svgs/play.svg" width="20" height="20" alt="Play"/>`;
+const pauseIcon = ` <img src="./assets/svgs/pause.svg" width="20" height="20" alt="Pause"/>`;
 
-const retrievedMusicFiles = [];
+const homeRender = `<img class="home-video" src="./assets/gifs/bmo-dancing.gif"/>`;
+
+function renderHome() {
+  content.innerHTML = homeRender;
+}
+function renderMusic() {
+  const musicFilesRender = retrievedMusicFiles
+    .map((files) => `<div onclick="playMusic('${files}')">` + files + "</div>")
+    .join(" ");
+  console.log("Music Files Render", musicFilesRender);
+  const filesRender = `<div class="" onclick="selectMusicFolder()" ><span>+</span> Add new source</div> <div>${musicFilesRender}</div> `;
+  content.innerHTML = filesRender;
+}
+
 async function selectMusicFolder() {
   const selectedFolder = await window.electronAPI.selectMusicFolder();
   if (!selectedFolder) {
@@ -12,11 +33,44 @@ async function selectMusicFolder() {
   } else {
     console.log(selectedFolder);
     const musicFiles = await window.electronAPI.getMusicFiles(selectedFolder);
-    console.log("Music", musicFiles);
+    retrievedMusicFiles = musicFiles;
+    defaultPath = selectedFolder.filePaths[0] + "/";
+    renderMusic();
   }
 }
-selectMusicFolder();
 
-const states = "Home";
+function playMusic(filePath) {
+  console.log(`${defaultPath + filePath}`);
+  audioPlayer.src = `${defaultPath + filePath}`;
+  audioPlayer.play();
+  displayScreen.textContent = `${filePath}`;
+  playBtn.innerHTML = pauseIcon;
+  playing = true;
+}
 
-const homeRender = `<div class="" onclick =${selectMusicFolder} ><span>+</span> Add new source</div> ${musi}`;
+content.innerHTML = homeRender;
+playBtn.innerHTML = playIcon;
+
+function playPause() {
+  if (playing) {
+    audioPlayer.pause();
+    playing = false;
+    playBtn.innerHTML = playIcon;
+  } else {
+    audioPlayer.play();
+    playing = true;
+    playBtn.innerHTML = pauseIcon;
+  }
+}
+
+homeNav.addEventListener("click", () => {
+  renderHome();
+});
+
+musicNav.addEventListener("click", () => {
+  renderMusic();
+});
+
+playBtn.addEventListener("click", () => {
+  playPause();
+});
